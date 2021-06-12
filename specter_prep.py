09878e575717d58582d4a4bd5c36982f4eedde59
Ronald Seoh <ronaldseoh@icloud.com>
2021-06-12 03:32:56 -0400

    specter_prep.py: bug fix

--- specter_prep.py
+++ specter_prep.py
@@ -101,7 +101,7 @@ def get_citations_by_ids(citation_data_direct, ids):
 
     for paper_id in ids:
         try:
-            citations.union(set(citation_data_direct[paper_id].keys()))
+            citations = citations.union(set(citation_data_direct[paper_id].keys()))
         except:
             print('paper {} not in citation_data_direct. Skipping.'.format(paper_id))
 
