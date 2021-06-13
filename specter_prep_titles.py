d5a7fccbff805cce042700bd58cb58a41e0d703a
Ronald Seoh <ronaldseoh@icloud.com>
2021-06-13 10:34:44 -0400

    specter_prep_titles.py: speedup attempt

--- specter_prep_titles.py
+++ specter_prep_titles.py
@@ -28,8 +28,8 @@ def parse_metadata_shard(data_dir, shard_num, ids):
         desc="#" + "{}".format(shard_num).zfill(6),
         position=shard_num+1)
 
-    for paper in reader.iter(skip_invalid=True):
-        for paper_id in ids:
+    for paper_id in ids:
+        for paper in reader.iter(skip_invalid=True):
             if paper['paper_id'] == paper_id:
                 output_title[paper['paper_id']] = paper['title']
                 break
