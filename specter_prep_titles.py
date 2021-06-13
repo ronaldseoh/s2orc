ecf4ad016d28bc2749b0741b555555c8b8d91b15
Ronald Seoh <ronaldseoh@icloud.com>
2021-06-13 03:00:03 -0400

    Revert "specter_prep_titles.py: loop fix"
    This reverts commit 0535bb675b442b208306627aaaf17f9c47d90a11.

--- specter_prep_titles.py
+++ specter_prep_titles.py
@@ -25,11 +25,10 @@ def parse_metadata_shard(data_dir, shard_num, ids):
         desc="#" + "{}".format(shard_num).zfill(6),
         position=shard_num+1)
 
-    for paper_id in ids:
-        for paper in reader.iter(skip_invalid=True):
-            if paper['paper_id'] == paper_id:
-                output_title[paper['paper_id']] = paper['title']
-                break
+    for paper in reader.iter(skip_invalid=True):
+
+        if paper['paper_id'] in ids:
+            output_title[paper['paper_id']] = paper['title']
 
         pbar.update(1)
 
